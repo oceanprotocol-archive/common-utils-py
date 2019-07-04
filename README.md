97b1e624f0871ac1126a0dbd85efeb9b9c714305
eruizgar91 <enrique@oceanprotocol.com>
2019-07-04 10:07:16 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
+[![banner](https://raw.githubusercontent.com/oceanprotocol/art/master/github/repo-banner%402x.png)](https://oceanprotocol.com)
+
 # common-utils-py
 Library including all the common functionalities used in Python projects
+
+---
+
+## Table of Contents
+
+  - [Features](#features)
+  - [Prerequisites](#prerequisites)
+  - [Quick-start](#quick-start)
+        - [Code style](#code-style)
+        - [Testing](#testing)
+  - [License](#license)
+
+---
+
+## Features
+
+
+## Prerequisites
+
+Python 3.6
+
+## Quick-start
+
+Install Squid:
+
+```
+pip install squid-py
+```
+
+#### Code style
+
+The information about code style in python is documented in this two links [python-developer-guide](https://github.com/oceanprotocol/dev-ocean/blob/master/doc/development/python-developer-guide.md)
+and [python-style-guide](https://github.com/oceanprotocol/dev-ocean/blob/master/doc/development/python-style-guide.md).
+
+#### Testing
+
+Automatic tests are setup via Travis, executing `tox`.
+Our test use pytest framework.
+
+## License
+
+```
+Copyright 2018 Ocean Protocol Foundation Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ setup.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+#  Copyright 2018 Ocean Protocol Foundation
+#  SPDX-License-Identifier: Apache-2.0
+
+import os
+from os.path import join
+
+from setuptools import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('CHANGELOG.md') as history_file:
+    history = history_file.read()
+
+# Installed by pip install common-utils-py
+# or pip install -e .
+install_requirements = [
+
+]
+
+# Required to run setup.py:
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = [
+    'codacy-coverage',
+    'coverage',
+    'docker',
+    'mccabe',
+    'pylint',
+    'pytest',
+    'pytest-watch',
+    'tox',
+]
+
+# Possibly required by developers of common-utils-py:
+dev_requirements = [
+    'bumpversion',
+    'pkginfo',
+    'twine',
+    'watchdog',
+]
+
+docs_requirements = [
+    'Sphinx',
+    'sphinxcontrib-apidoc',
+]
+
+packages = []
+for d, _, _ in os.walk('common_utils_py'):
+    if os.path.exists(join(d, '__init__.py')):
+        packages.append(d.replace(os.path.sep, '.'))
+
+setup(
+    author="leucothia",
+    author_email='devops@oceanprotocol.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+    ],
+    description="🐳 Library including all the common functionalities used in Python projects",
+    extras_require={
+        'test': test_requirements,
+        'dev': dev_requirements + test_requirements + docs_requirements,
+        'docs': docs_requirements,
+    },
+    install_requires=install_requirements,
+    license="Apache Software License 2.0",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords='common-utils-py',
+    name='common-utils-py',
+    packages=packages,
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/oceanprotocol/common-utils-py',
+    version='0.0.1',
+    zip_safe=False,
+)
