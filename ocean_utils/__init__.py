dc83c1fbf26bd3cccf9d9fb6459010572eff61bb
ssallam <samer@oceanprotocol.com>
2019-11-20 23:40:29 +0100

    Bump version: 0.2.5 → 0.2.6

--- ocean_utils/__init__.py
+++ ocean_utils/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.5'
+__version__ = '0.2.6'
 
 #  Copyright 2018 Ocean Protocol Foundation
 #  SPDX-License-Identifier: Apache-2.0
--- setup.py
+++ setup.py
@@ -85,6 +85,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/common-utils-py',
-    version='0.2.5',
+    version='0.2.6',
     zip_safe=False,
 )
