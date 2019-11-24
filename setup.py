ddba99b0dac6896d998762d330157192c9103b14
ssallam <samer@oceanprotocol.com>
2019-11-24 17:21:23 +0100

    Bump version: 0.2.7 → 0.3.0

--- setup.py
+++ setup.py
@@ -85,6 +85,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/common-utils-py',
-    version='0.2.7',
+    version='0.3.0',
     zip_safe=False,
 )
